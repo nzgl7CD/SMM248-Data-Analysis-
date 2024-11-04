3bd533043d1ea6ca43e234884df356425d0de72c
Sebastian Veum <sebastian.veum@gmail.com>
2024-11-04 17:35:23 +0000

    adjusted BS

--- extras/stock_analysis.py
+++ extras/stock_analysis.py
@@ -27,56 +27,6 @@ class Stocks:
     def get_dataframe(self):
         return self.df
 
-class Options:
-    def __init__(self,dataframe) -> None:
-        self.df=dataframe
-        self.add_option_premiums()
-    
-    def add_option_premiums(self):
-        '''
-        Using Black-Scholes
-        TODO: fix teh calculation by splitting it in pieces
-        '''
-        rf = 0.05  # Risk-free rate
-
-        # Define strikes
-        self.df['Call_Strike'] = self.df['Close'] + 5  # Strike 5 above spot 
-        self.df['Put_Strike'] = self.df['Close'] - 5  # Strike 5 below spot 
-
-        for idx in range(len(self.df)):
-            # Check if any required value is None (or NaN)
-            if pd.isnull(self.df['Close'].iloc[idx]) or pd.isnull(self.df['Call_Strike'].iloc[idx]) or pd.isnull(self.df['Put_Strike'].iloc[idx]) or pd.isnull(self.df['ann_vol'].iloc[idx]):
-                continue  # Skip to the next iteration if any value is None
-
-            # Calculate d1 and d2 for call option
-            d1_call = (np.log(self.df['Close'].iloc[idx] / self.df['Call_Strike'].iloc[idx]) + 
-                    (rf + 0.5 * self.df['ann_vol'].iloc[idx]**2) * (30 / 252)) / \
-                    (self.df['ann_vol'].iloc[idx] * np.sqrt(30 / 252))
-
-            d2_call = d1_call - self.df['ann_vol'].iloc[idx] * np.sqrt(30 / 252)
-            norm_d1_call = norm.cdf(d1_call)
-            norm_d2_call = norm.cdf(d2_call)
-            
-
-            # Calculate Call Premium
-            self.df.at[idx, 'Call_Premium'] = (self.df['Close'].iloc[idx] * norm_d1_call -
-                                                self.df['Call_Strike'].iloc[idx] * np.exp(-rf * (30 / 252)) * norm_d2_call)
-
-            # Calculate d1 and d2 for put option
-            d1_put = (np.log(self.df['Close'].iloc[idx] / self.df['Put_Strike'].iloc[idx]) + 
-                    (rf + 0.5 * self.df['ann_vol'].iloc[idx]**2) * (30 / 252)) / \
-                    (self.df['ann_vol'].iloc[idx] * np.sqrt(30 / 252))
-
-            d2_put = d1_put - self.df['ann_vol'].iloc[idx] * np.sqrt(30 / 252)
-            norm_d1_put = norm.cdf(-d1_put)  # Corrected sign
-            norm_d2_put = norm.cdf(-d2_put)  # Corrected sign
-
-            # Calculate Put Premium
-            self.df.at[idx, 'Put_Premium'] = (self.df['Put_Strike'].iloc[idx] * np.exp(-rf * (30 / 252)) * norm_d2_put - 
-                                                self.df['Close'].iloc[idx] * norm_d1_put)
-        # print(self.df[['Call_Premium']])
-
-    
     
 def get_stock_data(ticker:str, start_date,end_date):
     data_collected=yf.Ticker(ticker)
@@ -90,8 +40,29 @@ data_input=pd.DataFrame(get_stock_data('META', start_date, end_date))
 object=Stocks(data_input)
 adjusted_dataframe=object.get_dataframe()
 
-option_object=Options(adjusted_dataframe)
 
+
+def BS_CALL(self,S, K, T, r, sigma):
+    N = norm.cdf
+    d1 = (np.log(S/K) + (r + sigma**2/2)*T) / (sigma*np.sqrt(T))
+    d2 = d1 - sigma * np.sqrt(T)
+    return S * N(d1) - K * np.exp(-r*T)* N(d2)
+
+def BS_PUT(self,S, K, T, r, sigma):
+    N = norm.cdf
+    d1 = (np.log(S/K) + (r + sigma**2/2)*T) / (sigma*np.sqrt(T))
+    d2 = d1 - sigma* np.sqrt(T)
+    return K*np.exp(-r*T)*N(-d2) - S*N(-d1)
+
+
+K = 100
+r = 0.1
+T = 1
+sigma = 0.3
+S = np.arange(60,140,0.1)
+
+calls = [BS_CALL(s, K, T, r, sigma) for s in S]
+puts = [BS_PUT(s, K, T, r, sigma) for s in S]
 
 
 
